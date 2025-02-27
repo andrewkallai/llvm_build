ff6ea5610c08d553940be8396413f5fae0cb7d97
Andrew Kallai <kallai.andrew@gmail.com>
2025-02-27 18:43:12 -0500

    Adding new changes to input-gen build and adding build for libcxx.

--- darwin_libcxx_build.sh
+++ darwin_libcxx_build.sh
@@ -9,28 +9,28 @@ export PATH="$HOME/cmake-3.30.0-linux-x86_64/bin:$PATH"
 #export LD_LIBRARY_PATH="/lustre/schandra_crpl/sw/LLVM/previous_llvm_install/lib:$LD_LIBRARY_PATH"
 
 vpkg_require ninja
-vpkg_require llvm/17.0.6
+vpkg_require gcc/12.2.0
 vpkg_require python/3.8.6
 
 
-SRC=/lustre/schandra_crpl/sw/LLVM/llvm-ml
-INSTALL=$HOME/llvm-input-gen-install
+SRC=/lustre/schandra_crpl/sw/LLVM
+INSTALL=$HOME/llvm-libcxx-install
 
 LLVM_PROJECTS="clang;lld;compiler-rt"
 
 mkdir -p $INSTALL
 #cd $SRC 
 cd $HOME
-mkdir -p llvm_build_tmp
+mkdir -p llvm_build_libcxx
 #cd ./build
-cd $HOME/llvm_build_tmp
+cd $HOME/llvm_build_libcxx
 
 cmake ${SRC}/llvm-project/llvm -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=${INSTALL} \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
-  -DCMAKE_CXX_USING_LINKER_MODE="-fuse-ld=lld" \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_CXX_COMPILER=g++ \
+#  -DCMAKE_CXX_USING_LINKER_MODE="-fuse-ld=lld" \
   -DCMAKE_LINKER_TYPE=LLD \
 #  -DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
 #  -DLLVM_USE_LINKER=lld \
@@ -50,7 +50,7 @@ cmake --build .
 #cmake --build . --target install/parallel
 cmake --build . --target install
 
-./bin/llvm-lit -v test/tools/input-gen/
+#./bin/llvm-lit -v test/tools/input-gen/
 
 if [ $? -eq 0 ]; then
   #cd ..
